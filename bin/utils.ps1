ddc69177be471213c18d308a3d768da502221dca
akirco <earlfx@163.com>
2024-08-15 21:53:23 +0800

    UPDATE

---
+++ bin/utils.ps1
@@ -0,0 +1,18 @@
+function get_installer_info([string]$app) {
+    $rootDir = $app.ToLower()[0]
+    $parts = $app -split '/'
+    $id = $parts -join '.'
+    $versionList = [array]((Invoke-WebRequest -Uri "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/$($rootDir)/$($app)").Content | ConvertFrom-Json  | ForEach-Object { $_.name } | Where-Object { $_ -notmatch '^\.' } | Sort-Object { try { [version]$_ }catch {} } -Descending)
+    $installer_yaml = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/winget-pkgs/master/manifests/$($rootDir)/$($app)/$($versionList[0])/$($id).installer.yaml"
+
+    $installer_info = ConvertFrom-Yaml $installer_yaml.Content
+    foreach ($_ in $installer_info.Installers) {
+        $arch = $_.Architecture
+        $type = [regex]::Match($_.InstallerUrl, '\.(\w+)$').Groups[1].Value
+        $res = if ($type) { $arch + '_' + $type }else { $arch }
+        if ($arch) {
+            $installer_info.$res = $_
+        }
+    }
+    $installer_info
+}
