d47068685e5700bb355fbc9b0030987bb3ba9c3d
akirco <earlfx@163.com>
2024-11-21 11:43:03 +0800

    fixs

--- bin/utils.ps1
+++ bin/utils.ps1
@@ -3,10 +3,14 @@ function get_installer_info([string]$app) {
     $parts = $app -split '/'
     $id = $parts -join '.'
     $versionList = [array]((Invoke-WebRequest -Uri "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/$($rootDir)/$($app)").Content | ConvertFrom-Json  | ForEach-Object { $_.name } | Where-Object { $_ -notmatch '^\.' } | Sort-Object { try { [version]$_ }catch {} } -Descending)
-    Write-Host "version: $versionList[0]"
-    $installer_yaml = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/winget-pkgs/master/manifests/$($rootDir)/$($app)/$($versionList[0])/$($id).installer.yaml"
+    Write-Host "version: $($versionList[0])"
+    $url = "https://raw.githubusercontent.com/microsoft/winget-pkgs/master/manifests/$($rootDir)/$($app)/$($versionList[0])/$($id).installer.yaml"
+    Write-Host "URL:$url"
+    $installer_yaml = Invoke-WebRequest -Uri $url -Verbose
 
     $installer_info = ConvertFrom-Yaml $installer_yaml.Content
+
+    Write-Host $installer_info
     foreach ($_ in $installer_info.Installers) {
         $arch = $_.Architecture
         $type = [regex]::Match($_.InstallerUrl, '\.(\w+)$').Groups[1].Value
